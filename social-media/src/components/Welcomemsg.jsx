bab5a21a2e199291ad8335313e5a0181d5d60ea6
hyma9 <hymavathidwarasala@gmail.com>
2025-10-28 15:57:34 +0530

    Removed social-media and social-media2 folders

--- social-media/src/components/Welcomemsg.jsx
+++
@@ -1,9 +0,0 @@
-const Welcomemsg=({postsClick})=>{
-    return (
-        <center className="welcome-msg">
-            <h1>There are no posts</h1>
-            
-        </center>
-    )
-}
-export default Welcomemsg;
